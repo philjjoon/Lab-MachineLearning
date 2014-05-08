e03b550c7b53841bc68c7dac83db91f4d9f8e8b9
Budi Yanto <budiyanto@live.com>
2014-05-08 20:48:59 +0200

    Finish Assignment 3 and Assignment 5

--- ProblemSet-01/testsNstubs/ps1_implementation.py
+++ ProblemSet-01/testsNstubs/ps1_implementation.py
@@ -44,7 +44,15 @@ def pca(X, m):
 
 
 def gammaidx(X, k):
-    ''' your header here!
+    ''' Calculate gamma index for each data point
+
+    Usage: 
+        y = gammaidx(X, k)
+    Parameters:
+        X: (d x n)  array of column vectors
+        k: (k)      a scalar used as the number of neighbours
+    Returns:
+        y: (1 x n)  1D array of gamma index for each data point
     '''
     # Calculate distance matrix
     D = distmat(X, X)
@@ -70,7 +78,7 @@ def distmat(M, X):
     Returns:
         D: (k x n) array of distances between the vectors in M and X
     '''
-    
+
     M2 = np.sum(M**2, axis=0)[:, np.newaxis]
     MX = np.dot(M.T, X)
     X2 = np.sum(X**2, axis=0)[np.newaxis, :]
@@ -78,8 +86,8 @@ def distmat(M, X):
     D2[D2<=0] = np.nan # not using the distance to itself and other minus distance
     return np.sqrt(D2)
 
-#TODO: take care of regularization
-def lle(X, m, n_rule, param, tol=1e-2):
+
+def lle(X, m, n_rule, param, tol=1e-3):
     ''' your header here!
     '''
 
@@ -88,73 +96,110 @@ def lle(X, m, n_rule, param, tol=1e-2):
     # Step 1: Finding the nearest neighbours
     print 'Step 1: Finding the nearest neighbours by rule ' + n_rule
     
+    # Calculate distance matrix
+    DM = distmat(X, X)
+
     if n_rule == 'knn':
-        # Calculate distance matrix
-        DM = distmat(X, X)
-    
+        
         # Sort and get the k-nearest neighbours for each data point
-        inds = np.argsort(DM, axis=0)[:m, :] 
+        neighbours = np.argsort(DM, axis=0)[:param, :] 
 
     elif n_rule == 'eps-ball':
+        
         print 'eps-ball'
+
+
     else:
         print 'Rule is unknown. Please choose either knn or eps-ball'
 
     # Step 2: local reconstruction weights
     print 'Step 2: local reconstruction weights'
     
-    if m <= d:
+    if param <= d:
         tol = 0
         
-    '''
-    W = np.zeros([m, n])                                # Matrix W to store the weights
+    
+    W = np.zeros([param, n], dtype=float)                       # Matrix W to store the weights
     for i in range(n):
-        Z = X[:, inds[:, i]] - X[:, i][:, np.newaxis]   # Create matrix Z consisting of all neighbours of Xi
-        C = np.dot(Z.T, Z)                              # Compute local covariance
-        W[:, i] = np.dot(la.inv(C), np.ones([m, 1]))    # Solve C * Wi = 1
-        W[:, i] = W[:, i] / np.sum(W[:, i])             # Enforce sum(Wi) = 1
-    '''
-    W = np.zeros([n, n])
-    for i in range(n):
-        Z = X[:, inds[:, i]] - X[:, i][:, np.newaxis]   # Create matrix Z consisting of all neighbours of Xi
-        C = np.dot(Z.T, Z)                              # Compute local covariance
-        C = C + (np.eye(m, m) * tol * np.trace(C))      # Regularization if m > D
-        w = np.dot(la.pinv(C), np.ones([m, 1]))          # Solve C * Wi = 1
-        w = w / np.sum(w)                               # Enforce sum(Wi) = 1
-        W[inds, i] = w
+        Z = X[:, neighbours[:, i]] - X[:, i][:, np.newaxis]     # Create matrix Z consisting of all neighbours of Xi  
+        C = np.dot(Z.T, Z)                                      # Compute local covariance        
+        C = C + (np.eye(param, param) * tol * np.trace(C))      # Regularization if param > d
+        W[:, i] = (la.solve(C, np.ones([param, 1]))).T          # Solve C * Wi = 1
+        W[:, i] = W[:, i] / np.sum(W[:, i])                     # Enforce sum(Wi) = 1
+        
 
     # Step 3: compute embedding
     print 'Step 3: compute embedding'
-    M = np.eye(n, n) - W - W.T + np.dot(W.T, W)
+    M = np.eye(n, n, dtype=float)
+    for i in range(n):
+        w = W[:, i]
+        j = neighbours[:, i]
 
-    E, V = la.eigh(M)
+        M[i, j] -= w
+        M[j, i] -= w
+        w = w[:, np.newaxis]
+        M[np.ix_(j, j)] += np.dot(w, w.T)
+
+
+    E, V = la.eig(M)
     inds = np.argsort(E)
     E = E[inds]
     V = V[:, inds]
-    
+
     Y = V[:, 1:(m+1)].T * np.sqrt(n)
 
     return Y
 
-# Helper functions
+
+# Various helper functions
 def load_data(dataset='usps'):
+    ''' Load various dataset '''
+
     if dataset == 'usps':
         data = np.load('usps.npz')
         images = data['data_patterns']
         images = images - np.min(images)
         images = vec2im(images, (16, 16))
         labels = data['data_labels']
+        return images.astype('float'), labels
     if dataset == 'banana_data':
         print 'Banana'
-    return images.astype('float'), labels
+
+    if dataset == 'flatroll':
+        X = np.load('flatroll_data.npz')
+        data = X['Xflat']
+        true_emb = X['true_embedding']
+
+    if dataset == 'fishbowl':
+        X = np.load('fishbowl_data.npz')
+        data = X['x_noisefree']
+        true_emb = X['z']
+
+    if dataset == 'fishbowl_dense':
+        X = np.load('fishbowl_dense.npz')
+        data = X['X']
+        true_emb = None
+
+    if dataset == 'swissroll':
+        X = np.load('swissroll_data.npz')
+        data = X['x_noisefree']
+        true_emb = X['z']
+
+    return data, true_emb
+
+    
 
 def im2vec(images):
+    ''' Convert images 3D to vector 2D '''
+
     x, y, n = images.shape
     images = images.reshape(x*y, n)
     fact = (x, y)
     return images, fact
 
 def vec2im(images, fact=False):
+    ''' Convert vector 2D to images 3D '''
+
     xy, n = images.shape
     if fact:
         x, y = fact
@@ -168,9 +213,8 @@ def vec2im(images, fact=False):
     return images
 
 def visualize_data(ax, data, subs=(3,4)):
-    ''' visualize a set of images.
-    '''
-    # your code here
+    ''' Visualize a set of images '''
+
     X, Y, N = data.shape
     x, y = subs
     matrix = np.ones([x*X, y*Y])

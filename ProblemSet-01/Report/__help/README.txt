69ddb30646c68a6890baa911ffce8161d3d9be94
Budi Yanto <budiyanto@live.com>
2014-05-10 11:47:50 +0200

    Final version of ps1_application and add latex of the report

---
+++ ProblemSet-01/Report/__help/README.txt
@@ -0,0 +1,11 @@
+If you are using Windows, you should install the following tools:
+
+1. Miktex (newest and full version in order to avoid problems) miktex.org/
+2. texniccenter www.texniccenter.org/
+
+For Linux following tools are interesting:
+
+-gedit
+-vi
+-rubber
+-...
--- ProblemSet-01/testsNstubs/ps1_implementation.py
+++ ProblemSet-01/testsNstubs/ps1_implementation.py
@@ -19,7 +19,17 @@ import pylab as pl
 
 
 def pca(X, m):
-    ''' your header here!
+    ''' Calculate principle components of given data set X
+
+    Usage:
+        Z, U, D = pca(X, m)
+    Parameters:
+        X: (d x n) array of column vectors
+        m: (scalar) number of component to be used
+    Returns:
+        Z: (m x n) matrix of the projected data points on feature vectors
+        U: (d x d) matrix contains the principle directions
+        D: (1 x d) 1D array contains the principle values sorted in descending order
     '''
 
     # Substract X from its mean
@@ -36,8 +46,10 @@ def pca(X, m):
     D = D[inds[::-1]]
     U = U[:, inds[::-1]]
     
-    # Form the feature vector
+    # Form the feature vectors
     U_reduction = U[:, :m]
+
+    # Project the zero-mean X to feature vectos
     Z = np.dot(U_reduction.T, X_zero_mean)
     
     return Z, U, D
@@ -50,25 +62,27 @@ def gammaidx(X, k):
         y = gammaidx(X, k)
     Parameters:
         X: (d x n)  array of column vectors
-        k: (k)      a scalar used as the number of neighbours
+        k: (scalar) number of neighbours
     Returns:
         y: (1 x n)  1D array of gamma index for each data point
     '''
+
     # Calculate distance matrix
     D = distmat(X, X)
     
     # Sort the distance matrix
     D = np.sort(D, axis=0)
 
-    # Take only the k-nearest
+    # Take only the k-nearest neighbours
     D = D[:k, :] # do not use the distance between the point and itself
 
+    # Calculate the mean of the k-nearest neighbours
     y = np.mean(D, axis=0, keepdims=True)
 
     return y
 
 def distmat(M, X):
-    ''' calculates a distance matrix between M and X
+    ''' Calculate a distance matrix between M and X
         
     Usage:
         D = distmat( M, X )
@@ -88,9 +102,21 @@ def distmat(M, X):
 
 
 def lle(X, m, n_rule, param, tol=1e-3):
-    ''' your header here!
+    ''' Calculate locally linear embedding of a given data set X
+
+    Usage:
+        Y = lle(X, m, n_rule, param, tol)
+    Parameters:
+        X: (d x n) array of column vectors as input data set
+        m: (scalar) dimension of the resulting embedding
+        n_rule: (string) rule used to build the neighbourhood graph
+        param: (scalar) the corresponding parameter for n_rule
+        tol: (scalar) regularization parameter
+    Returns:
+        Y: (m x n) array of column vectors as resulting embedding
     '''
 
+    # Get the dimension and total number of data points
     d, n = X.shape
 
     # Step 1: Finding the nearest neighbours
@@ -106,35 +132,54 @@ def lle(X, m, n_rule, param, tol=1e-3):
 
     elif n_rule == 'eps-ball':
         
+        # Get the neighbours for each data point based on eps-ball
         neighbours = []
         for i in range(n):
             inds = np.where(DM[:, i] <= param)[0]
             neighbours.append(inds)
 
     else:
-        print 'Rule is unknown. Please choose either knn or eps-ball'
+        raise Exception('Rule is unknown. Please choose either knn or eps-ball!')
+
 
     # Step 2: local reconstruction weights
     print 'Step 2: local reconstruction weights'
     
+    # Only use regularization if param <= d
     if param <= d:
         tol = 0
+
+    # Matrix W to store the weights
+    W = np.zeros([n, n], dtype=float)                       
     
-    W = np.zeros([n, n], dtype=float)                       # Matrix W to store the weights
-    for i in range(n):
+    for i in range(n): # Iterate through all data points
+
+        # Calculate neighbours based on n_rule
         if n_rule == 'knn':
             neighbour = neighbours[:, i]
         elif n_rule == 'eps-ball':
             neighbour = neighbours[i]
 
-        Z = X[:, neighbour] - X[:, i][:, np.newaxis]        # Create matrix Z consisting of all neighbours of Xi  
-        C = np.dot(Z.T, Z)                                  # Compute local covariance        
-        C = C + (np.eye(len(neighbour), len(neighbour)) * tol * np.trace(C))      # Regularization if param > d
-        W[neighbour, i] = la.solve(C, np.ones(len(neighbour)))          # Solve C * Wi = 1
-        W[neighbour, i] = W[neighbour, i] / np.sum(W[:, i])                     # Enforce sum(Wi) = 1
-        
+        # Create matrix Z consisting of all neighbours of Xi and shift them to origin
+        Z = X[:, neighbour] - X[:, i][:, np.newaxis]        
+
+        # Compute local covariance 
+        C = np.dot(Z.T, Z)
+
+        # Use regularization if param > d                         
+        C = C + (np.eye(len(neighbour), len(neighbour)) * tol * np.trace(C))
+
+        # Solve C * Wi = 1     
+        W[neighbour, i] = la.solve(C, np.ones(len(neighbour)))
+
+        # Enforce sum(Wi) = 1          
+        W[neighbour, i] = W[neighbour, i] / np.sum(W[:, i])                     
+
+
     # Step 3: compute embedding
     print 'Step 3: compute embedding'
+
+    # Create a sparse matrix M
     M = np.eye(n, n, dtype=float)
     for i in range(n):
         if n_rule == 'knn':
@@ -148,19 +193,26 @@ def lle(X, m, n_rule, param, tol=1e-3):
         w = w[:, np.newaxis]
         M[np.ix_(j, j)] += np.dot(w, w.T)
 
+    # Calculate eigenvectors and eigenvalues
     E, V = la.eig(M)
+
+    # Sort the eigenvectors and eigenvalues in ascending order
     inds = np.argsort(E)
     E = E[inds]
     V = V[:, inds]
+
+    # Take (m+1) smallest eigenctors, discarding the smallest one
     Y = V[:, 1:(m+1)].T * np.sqrt(n)
 
     return Y
 
 
-# Various helper functions
+''' Various helper functions '''
+
 def load_data(dataset='usps'):
     ''' Load various dataset '''
 
+    # Load usps data set
     if dataset == 'usps':
         data = np.load('usps.npz')
         images = data['data_patterns']
@@ -168,24 +220,33 @@ def load_data(dataset='usps'):
         images = vec2im(images, (16, 16))
         labels = data['data_labels']
         return images.astype('float'), labels
-    if dataset == 'banana_data':
-        print 'Banana'
 
+    # Load banana data set
+    if dataset == 'banana':
+        X = np.load('banana_data.npz')
+        data = X['data']
+        label = X['label']
+        return data, label
+
+    # Load flatroll data set
     if dataset == 'flatroll':
         X = np.load('flatroll_data.npz')
         data = X['Xflat']
         true_emb = X['true_embedding']
 
+    # Load fishbowl data set
     if dataset == 'fishbowl':
         X = np.load('fishbowl_data.npz')
         data = X['x_noisefree']
         true_emb = X['z']
 
+    # Load dense fishbowl data set
     if dataset == 'fishbowl_dense':
         X = np.load('fishbowl_dense.npz')
         data = X['X']
         true_emb = None
 
+    # Load swissroll data set
     if dataset == 'swissroll':
         X = np.load('swissroll_data.npz')
         data = X['x_noisefree']
@@ -201,6 +262,7 @@ def im2vec(images):
     x, y, n = images.shape
     images = images.reshape(x*y, n)
     fact = (x, y)
+
     return images, fact
 
 def vec2im(images, fact=False):
@@ -216,6 +278,7 @@ def vec2im(images, fact=False):
         y = xy / x
 
     images = images.reshape(x, y, n)
+    
     return images
 
 def visualize_data(ax, data, subs=(3,4)):
